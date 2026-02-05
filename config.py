cc2e646d32a5ac1664ff7a04eaa6c217f114d669
ㅤ𝛎 𝜄 ᥉ ᥴ᥆ ᥉ 𝜄 𝗍 𝛄 ✞ㅤ <166417113+viscity@users.noreply.github.com>
2026-02-05 21:39:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,7 +40,8 @@ UPSTREAM_REPO = getenv(
     "https://github.com/VISCITY/xbit.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-GIT_TOKEN = os.getenv("GIT_TOKEN", None)
+GIT_TOKEN = getenv(
+    "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/botscomplex")
